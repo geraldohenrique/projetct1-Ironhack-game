4e548e4dab47210c423357e92741c98ac3365188
Geraldo Melo <ghsmelo@gmail.com>
2022-11-01 15:40:27 -0300

    Acerto do endereco das imagens

--- src/game.js
+++ src/game.js
@@ -13,15 +13,15 @@ class QuebraCabeca{
         switch (this.imgBase){
             case 'treewayImg':
                 this.gridPadrao.push(
-                    '../img/treeway/treeway01.jpg',
-                    '../img/treeway/treeway02.jpg',
-                    '../img/treeway/treeway03.jpg',
-                    '../img/treeway/treeway04.jpg',
-                    '../img/treeway/treeway05.jpg',
-                    '../img/treeway/treeway06.jpg',
-                    '../img/treeway/treeway07.jpg',
-                    '../img/treeway/treeway08.jpg',
-                    '../img/treeway/treeway09.jpg'
+                    './img/treeway/treeway01.jpg',
+                    './img/treeway/treeway02.jpg',
+                    './img/treeway/treeway03.jpg',
+                    './img/treeway/treeway04.jpg',
+                    './img/treeway/treeway05.jpg',
+                    './img/treeway/treeway06.jpg',
+                    './img/treeway/treeway07.jpg',
+                    './img/treeway/treeway08.jpg',
+                    './img/treeway/treeway09.jpg'
                 )
                 copyGridPadrao =[...this.gridPadrao]
                 this.gridEmbaralhado = copyGridPadrao.sort(()=>{
@@ -30,15 +30,15 @@ class QuebraCabeca{
                 break;
             case 'townImg':
                 this.gridPadrao.push(
-                    '../img/town/town01.jpg',
-                    '../img/town/town02.jpg',
-                    '../img/town/town03.jpg',
-                    '../img/town/town04.jpg',
-                    '../img/town/town05.jpg',
-                    '../img/town/town06.jpg',
-                    '../img/town/town07.jpg',
-                    '../img/town/town08.jpg',
-                    '../img/town/town09.jpg'
+                    './img/town/town01.jpg',
+                    './img/town/town02.jpg',
+                    './img/town/town03.jpg',
+                    './img/town/town04.jpg',
+                    './img/town/town05.jpg',
+                    './img/town/town06.jpg',
+                    './img/town/town07.jpg',
+                    './img/town/town08.jpg',
+                    './img/town/town09.jpg'
                 )
                 copyGridPadrao =[...this.gridPadrao]
                 this.gridEmbaralhado = copyGridPadrao.sort(()=>{
@@ -47,15 +47,15 @@ class QuebraCabeca{
                 break;
             case 'arabMetalImg':
                 this.gridPadrao.push(
-                    '../img/arabmetal/arabmetal01.jpg',
-                    '../img/arabmetal/arabmetal02.jpg',
-                    '../img/arabmetal/arabmetal03.jpg',
-                    '../img/arabmetal/arabmetal04.jpg',
-                    '../img/arabmetal/arabmetal05.jpg',
-                    '../img/arabmetal/arabmetal06.jpg',
-                    '../img/arabmetal/arabmetal07.jpg',
-                    '../img/arabmetal/arabmetal08.jpg',
-                    '../img/arabmetal/arabmetal09.jpg'
+                    './img/arabmetal/arabmetal01.jpg',
+                    './img/arabmetal/arabmetal02.jpg',
+                    './img/arabmetal/arabmetal03.jpg',
+                    './img/arabmetal/arabmetal04.jpg',
+                    './img/arabmetal/arabmetal05.jpg',
+                    './img/arabmetal/arabmetal06.jpg',
+                    './img/arabmetal/arabmetal07.jpg',
+                    './img/arabmetal/arabmetal08.jpg',
+                    './img/arabmetal/arabmetal09.jpg'
                 )
                 copyGridPadrao =[...this.gridPadrao]
                 this.gridEmbaralhado = copyGridPadrao.sort(()=>{
